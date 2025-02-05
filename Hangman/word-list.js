a0d76b63509b72533a7a5aa541cd22ce3c5820d8
SurekhaReddy-D <desamsurekha@gmail.com>
2025-02-05 21:13:55 +0530

    Add files via upload

---
+++ Hangman/word-list.js
@@ -0,0 +1,43 @@
+const wordList = [
+{
+    word: "elephant",
+    hint: "A large mammal with big ears and a trunk."
+},
+{
+    word: "pyramid",
+    hint: "Ancient triangular structures in Egypt."
+},
+
+{
+    word: "guitar",
+    hint: "A musical instrument with strings."
+},
+{
+    word: "chocolate",
+    hint: "A sweet treat made from cocoa beans."
+},
+{
+    word: "volcano",
+    hint: "A mountain that erupts lava."
+},
+{
+    word: "submarine",
+    hint: "A ship that travels underwater."
+},
+{
+    word: "rainbow",
+    hint: "A colorful arc that appears after rain."
+},
+{
+    word: "kangaroo",
+    hint: "A marsupial from Australia that hops."
+},
+{
+    word: "helicopter",
+    hint: "A flying vehicle with rotating blades."
+},
+{
+    word: "telescope",
+    hint: "An instrument used to observe distant stars."
+},   
+];
